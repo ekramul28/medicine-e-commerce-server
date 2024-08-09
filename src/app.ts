bd41098a043fa1e76db360f64b15122d9fbc5187
Ekramul <mdekramulhassan168@gmail.com>
2024-08-09 21:29:18 +0600

    server setup added with error handeling

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application } from "express";
+import notFound from "./app/middlewares/notFound";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import router from "./app/routes";
+const app: Application = express();
+import cookieParser from "cookie-parser";
+
+//parsers
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// app.use("/api/v1", router);
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
