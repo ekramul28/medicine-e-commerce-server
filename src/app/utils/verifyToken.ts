f0dc32a29a9a26657f98af955bc66e072af41fb7
Ekramul <mdekramulhassan168@gmail.com>
2024-11-29 20:32:30 +0600

    some code added

--- src/app/utils/verifyToken.ts
+++ src/app/utils/verifyToken.ts
@@ -1,17 +1,17 @@
-import httpStatus from 'http-status';
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import AppError from '../errors/AppError';
-import config from '../config';
+import httpStatus from "http-status";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../errors/AppError";
+import config from "../config";
 
 const verifyToken = (token: string) => {
   let decoded;
   try {
     decoded = jwt.verify(
       token,
-      config.jwt_access_secret as string,
+      config.jwt_access_secret as string
     ) as JwtPayload;
   } catch (error) {
-    throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized!");
   }
   return decoded;
 };
